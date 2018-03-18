92303d22ea135706097e3a455e5ef6443ffa712e
Jarbas Silva Filho <jarbas.silva.f@gmail.com>
2018-03-18 16:26:29 -0300

    Implementando o padrao TemplateMethod no calculo de impostos com condicoes de valores maximos e minimos

--- impostos.py
+++ impostos.py
@@ -1,3 +1,26 @@
+from abc import ABC, abstractclassmethod
+
+
+class TemplateCalculaImpostosMaximoMinimo(ABC):
+
+    def calcula(self, orcamento):
+        if self.condicao_maxima(orcamento):
+            return self.calcula_condicao_maxima(orcamento)
+        return self.calcula_condicao_minima(orcamento)
+
+    @abstractclassmethod
+    def condicao_maxima(self, orcamento):
+        pass
+
+    @abstractclassmethod
+    def calcula_condicao_maxima(self, orcamento):
+        pass
+
+    @abstractclassmethod
+    def calcula_condicao_minima(self, orcamento):
+        pass
+
+
 class ISS(object):
 
     def calcula(self, orcamento):
@@ -8,3 +31,34 @@ class ICMS(object):
 
     def calcula(self, orcamento):
         return orcamento.valor * 0.06
+
+
+class ICMP(TemplateCalculaImpostosMaximoMinimo):
+
+    def condicao_maxima(self, orcamento):
+        return orcamento.valor > 500
+
+    def calcula_condicao_maxima(self, orcamento):
+        return orcamento.valor * 0.1
+
+    def calcula_condicao_minima(self, orcamento):
+        return orcamento.valor * 0.08
+
+
+class IJM(TemplateCalculaImpostosMaximoMinimo):
+
+    def condicao_maxima(self, orcamento):
+        return orcamento.valor > 500 and self.tem_item_que_vale_cem(orcamento)
+
+    def calcula_condicao_maxima(self, orcamento):
+        return orcamento.valor * 0.05
+
+    def calcula_condicao_minima(self, orcamento):
+        return orcamento.valor * 0.03
+
+    def tem_item_que_vale_cem(self, orcamento):
+        for item in orcamento.obter_itens():
+            if item.valor >= 100:
+                return True
+        return False
+
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from calculador import CalculadorDeImpostos, CalculadorDeDescontos
 from orcarmento import Orcamento, Item
-from impostos import ISS, ICMS
+from impostos import ISS, ICMS, ICMP, IJM
 
 orcamento = Orcamento()
 
 orcamento.adiciona_item(Item('Item 1', 100))
-orcamento.adiciona_item(Item('Item 2', 50))
+orcamento.adiciona_item(Item('Item 2', 500))
 
 
 calculadorDeImpostos = CalculadorDeImpostos()
 calculadorDeDescontos = CalculadorDeDescontos()
 
+print("ICM")
 calculadorDeImpostos.realiza_calculo(orcamento, ISS())
+print("ICMS")
 calculadorDeImpostos.realiza_calculo(orcamento, ICMS())
 
-calculadorDeDescontos.realiza_calculo(orcamento)
+print("ICMP")
+calculadorDeImpostos.realiza_calculo(orcamento, ICMP())
+print("IJM")
+calculadorDeImpostos.realiza_calculo(orcamento, IJM())
+
